da56f24a8ec7a380e36ab029ef704df3220b5268
idosonaweb <jvieiraluzpiva@gmail.com>
2022-08-03 12:19:58 -0300

    commit

---
+++ core/conexao_mysql.php
@@ -0,0 +1,36 @@
+<?php
+
+    function conecta() : mysqli
+    {
+        $servidor = 'localhost' ;
+
+        $banco = 'blog' ;
+
+        $port = 3307 ;
+
+        $usuario = 'root' ;
+
+        $senha = '' ;
+
+        $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $port) ;
+
+        if (!$conexao) 
+        {
+            echo "Erro: Não foi possível conectar ao Mysql. " . PHP_EOL ;
+
+            echo "Debugging errno: " . mysqli_connect_error() . PHP_EOL ;
+
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL ;
+
+            return null ;
+        }
+
+        return $conexao ;
+    }
+
+    function desconecta ()
+    {
+        mysqli_close($conexao) ;
+    }
+
+?>
